a5aad19b27db090474b2b6916b5a3a2ee079825e
BPabey <buddimalp@gmail.com>
2020-05-23 22:59:40 +0530

    Initial commit

---
+++ app/src/main/java/com/osaka/tutorial04/MainActivity.java
@@ -0,0 +1,28 @@
+package com.osaka.tutorial04;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addButton = (Button) findViewById(R.id.addBtn);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
